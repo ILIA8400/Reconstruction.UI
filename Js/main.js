565546bb6a3fda240cc658156d02d991ec51bfab
ilia <iliaasgharhorri@gmail.com>
2024-09-22 16:03:07 +0330

    Hero and header was updated

--- Js/main.js
+++ Js/main.js
@@ -1,10 +1,11 @@
+// Menu
 $(function() {
     $('.nav-button').click(function(){
-        $('.nav-button').toggleClass('change')
-    })
-})
+        $('.nav-button').toggleClass('change');
+        $('.nav-menu').toggleClass('custom-navbar-ham');
+    });
+});
 
-// Menu
 $(window).scroll(function () { 
     let position = $(this).scrollTop();
     if (position > 200) {
@@ -12,6 +13,18 @@ $(window).scroll(function () {
     }
     else $('.nav-menu').removeClass('custom-navbar');
 });
+
+$(window).scroll(function () { 
+    let position = $(this).scrollTop();
+    if (position > 380) {
+        $('.banner-heading').addClass('banner-heading-back').removeClass('banner-heading');
+        $('.banner-par').addClass('banner-par-back').removeClass('banner-par');
+    } else {
+        $('.banner-heading-back').addClass('banner-heading').removeClass('banner-heading-back');
+        $('.banner-par-back').addClass('banner-par').removeClass('banner-par-back');
+    }
+});
+
 // end menu
 
 
@@ -92,5 +105,4 @@ for (let index = 0; index < 4; index++) {
     )
 }
 //end
-
 
